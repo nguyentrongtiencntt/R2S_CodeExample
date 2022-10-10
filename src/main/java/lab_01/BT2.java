0c1d11ff2cc947f4a632411597bdd157a46c5ffd
Nguyễn Trọng Tiến <nguyentrongtien@gmail.com>
2022-10-10 13:07:44 +0700

    finish BT2_Lab_01

---
+++ src/main/java/lab_01/BT2.java
@@ -0,0 +1,37 @@
+package lab_01;
+
+import java.util.Scanner;
+
+public class BT2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Package number input : ");
+        int pack = sc.nextInt();
+
+        System.out.print("Distance input : ");
+        int distance = sc.nextInt();
+
+        System.out.print("Working day (shift) input : ");
+        int shift = sc.nextInt();
+
+        System.out.print("Good rating feedback : ");
+        int feedback = sc.nextInt();
+
+        System.out.print("Call input : ");
+        int call = sc.nextInt();
+
+        System.out.print("Cash pay for employee : ");
+        int electric = sc.nextInt();
+
+        System.out.print("Cash pay more : ");
+        int other = sc.nextInt();
+
+        double Salary = (double)(pack * 50 + distance * 75)
+                        + (double)(shift * 50) * 0.1
+                        + (double)feedback + (double)call
+                        + (double)electric + (double)other;
+        System.out.print("---------------------------");
+        System.out.print("Employee Salary = " + Salary);
+    }
+}
