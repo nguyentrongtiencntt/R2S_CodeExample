a27d8fd92da86ba81857435d4523b898b92da025
Nguyễn Trọng Tiến <nguyentrongtien@gmail.com>
2022-10-10 10:34:28 +0700

    finish BT1

---
+++ src/main/java/lab_01/BT1.java
@@ -0,0 +1,25 @@
+package lab_01;
+
+import java.util.Scanner;
+
+public class BT1 {
+    public static void main(String[] args) {
+
+        double length = 0, width = 0, area = 0, cir = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("length input :");
+        length = sc.nextDouble();
+
+        System.out.print("width input :");
+        width = sc.nextDouble();
+
+        area = length * width;
+        cir = (length + width) * 2.0;
+
+        System.out.println("---------------" );
+        System.out.println("Area =" + area);
+        System.out.print("---------------" );
+        System.out.println("\nCircumference = " + cir);
+    }
+}
