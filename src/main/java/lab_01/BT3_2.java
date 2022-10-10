497fafdc7288785ab5d69e89b06c5bcc078c18ec
Nguyễn Trọng Tiến <nguyentrongtien@gmail.com>
2022-10-10 13:32:09 +0700

    finish BT3_2_Lab_01

---
+++ src/main/java/lab_01/BT3_2.java
@@ -0,0 +1,24 @@
+package lab_01;
+
+import java.util.Scanner;
+
+public class BT3_2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double fah = 0;
+        System.out.print("Celsius input :");
+        int celsius  = sc.nextInt();
+
+        // C/5 = (F-32)/9 => F = (C/5)*9 + 32
+        fah = (celsius/5)*9 + 32;
+
+        System.out.println("Fahrenheit temprature is : " + fah);
+
+
+
+
+
+    }
+}
